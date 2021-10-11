0325d3ac1554f7dc5849a49991443de78ac02b0b
FridaSpace <90274951+FridaSpace@users.noreply.github.com>
2021-10-11 18:08:03 +0300

    leffahakukone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movie, setMovie] = useState({});
+    const [search, setSearch] = useState('');
+    
+    const API_KEY = "TÄSTÄ POISTIN API KEYN";
+    const url = `http://www.omdbapi.com/?t=${search}&apikey=${API_KEY}`;
+
+    const getMovie = async()=>{
+      try {
+        const response = await fetch(url);
+         const data = await response.json()
+         setMovie(data);
+      } catch (error) {
+          console.error(error);
+      } 
+    }
+
+    const onInputChange = e =>{
+      setSearch(e.target.value);
+    }
+
+    useEffect(()=> {
+      getMovie();
+    }, []);
+
+
+  function checkResponse(data){
+     if(data.Response==="True"){
+       return(
+         <div>
+            <img src={data.Poster} alt=""/>
+            <h4 style={{fontFamily: "poppins", color: "purple"}}>Title: {data.Title}</h4>
+            <p style={{fontFamily: "poppins", color: "purple"}}>Year: {data.Year}</p>
+            <p style={{fontFamily: "poppins", color: "purple"}}>Writer: {data.Writer}</p>
+            
+         </div>
+       );
+    }
+      return (
+        <p>Eipä löytynyt leffaa!</p>
+      );
+    }
+
+   return(
+      <div style={{margin: 50}}>
+        <h1 style={{fontFamily: "poppins", color: "purple"}}>LEFFAHAKUKONE</h1>
+         <input type="text" style={{marginBottom: 50, color: "purple"}} value={search} onChange={onInputChange}/>
+         <button type="submit" style={{fontFamily: "poppins", color: "purple"}} onClick={getMovie}>Etsi parhaat leffat!</button>
+         <br></br>   
+         {checkResponse(movie)}
+      </div>
+   );
 }
 
 export default App;
